cec9013d2b7c443be6f9d6ead278b991d83f2327
Ihor-Samburenko <samburenko.igor@gmail.com>
2023-01-08 17:59:16 +0200

    fix 9 task add textContent

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,12 @@
 const Btn = document.querySelector(".change-color");
-const ColorEl = document.querySelector(".color");
+const changeColorEl = document.querySelector(".color");
 
 Btn.addEventListener("click", changeColor);
 
 function changeColor() {
   let hexColor = getRandomHexColor();
   document.body.style.backgroundColor = hexColor;
-  colorEL.textContent = hexColor;
+  changeColorEl.textContent = hexColor;
 }
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
