e25f9576f226adb70799ae6b87e10cccdd9be3e0
Ihor-Samburenko <samburenko.igor@gmail.com>
2023-01-08 17:55:26 +0200

    complete 8 and 9 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formsElement = event.currentTarget.elements;
+
+  const yourEmail = formsElement.email.value;
+  const yourPassword = formsElement.password.value;
+
+  if (yourEmail === "" || yourPassword === "") {
+    alert("Всі поля мають бути заповнені");
+  } else {
+    event.currentTarget.reset();
+  }
+
+  console.log("Email ", yourEmail);
+  console.log("Password ", yourPassword);
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const Btn = document.querySelector(".change-color");
+const ColorEl = document.querySelector(".color");
+
+Btn.addEventListener("click", changeColor);
+
+function changeColor() {
+  let hexColor = getRandomHexColor();
+  document.body.style.backgroundColor = hexColor;
+  colorEL.textContent = hexColor;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
